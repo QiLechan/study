f2356749eada063cc903942e140f58f59eafa08b
QiLechan <qilechan@outlook.com>
2024-12-10 00:18:23 +0800

    修复了12.09.a.c

--- c/oj/a.c
+++ c/oj/a.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int size;
 
 int printArr(int arr[])
 
 {
-
     for (int i = 0; i < size; i++)
-
     {
-
         printf("%d", arr[i]);
     }
-    free(arr);
     printf("\n");
-
     return 0;
 }
 
@@ -23,35 +19,27 @@ int generate(int arr[], int i, int count)
 {
 
     // 1的个数
-
     if (count == 0)
-
     {
         printArr(arr);
-
         return 0;
     }
 
     // 等于数组长度时退出
-
     if (i == size)
-
     {
         return 0;
     }
 
     // 如果改为0
-
     arr[i] = 0;
     generate(arr, i + 1, count);
 
     // 如果改为1
-
     arr[i] = 1;
     generate(arr, i + 1, count - 1);
 
     // 复原
-
     arr[i] = 0;
     return 0;
 }
@@ -61,11 +49,12 @@ int main(void)
 {
     int count;
     scanf("%d %d", &size, &count);
-    int *arr = (int *)malloc(size * sizeof(int));
-    for (int i = 0, num = 0; i < size; i++)
+    int* arr = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
     {
-        arr[i]  =  0;
+        arr[i] = 0;
     }
     generate(arr, 0, count);
+    free(arr);
     return 0;
 }
